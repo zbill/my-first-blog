6308c41ee612f33b85e03d438df4b66d4349eb31
zbill <zhangbin.0527@aliyun.com>
2019-02-11 23:00:03 +0800

    修改publish方法名

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)  # 定义外键时需要on_delete,cascade是级联删除
     title = models.CharField(max_length=200)
@@ -8,7 +9,7 @@ class Post(models.Model):
     create_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)  # blank针对表单，可以不填写内容，null针对数据库，字段可以为空（新建model对象时不会报错）
 
-    def pulish(self):
+    def publish(self):
         self.published_date = timezone.now()
         self.save()
 
