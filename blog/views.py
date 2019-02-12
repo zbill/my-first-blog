40b09cada59ac8ee4d3de72880e82246bf44d46c
zbill <zhangbin.0527@aliyun.com>
2019-02-12 10:28:16 +0800

    从数据库中获取已发布的博客展示在页面中

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
-    # 接受request参数，用render渲染模板中的blog/xxx.html模板
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')  # lte小于等于
+    return render(request, 'blog/post_list.html', {'posts': posts})  # 接受request参数，用render渲染模板中的blog/xxx.html模板
+
